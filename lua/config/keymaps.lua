2891e02681875200234239c792d5cf724b8e0557
Set27 <matvei.2002@mail.ru>
2024-02-11 12:49:49 +0300

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Set indentation options
+vim.opt.tabstop =  2
+vim.opt.softtabstop =  2
+vim.opt.shiftwidth =  2
+vim.opt.expandtab = true
+-- Uncomment the lines below if you want to enable autoindent and smartindent
+-- vim.opt.autoindent = true
+-- vim.opt.smartindent = true
+
+-- Define normal mode mappings
+-- Make sure to use 'n' for normal mode mappings
+vim.api.nvim_set_keymap('n', 'p', 'p=`]', {noremap = true})
+vim.api.nvim_set_keymap('n', 'P', 'P=`]', {noremap = true})
---
+++ lua/config/vscode_keymaps.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('n', 'p', 'p=`]', {noremap = true})
+vim.api.nvim_set_keymap('n', 'P', 'P=`]', {noremap = true})
